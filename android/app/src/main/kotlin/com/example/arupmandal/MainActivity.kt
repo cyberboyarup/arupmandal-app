04841e8d4166911e13e8b2cd1a2b0f4bf0b36522
Arup Mandal <110271890+cyberboyarup@users.noreply.github.com>
2022-10-01 07:46:06 +0530

    Arup Mandal

---
+++ android/app/src/main/kotlin/com/example/arupmandal/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.arupmandal
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/projects.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MyProject extends StatefulWidget {
+  const MyProject({Key? key}) : super(key: key);
+
+  @override
+  State<MyProject> createState() => _MyProjectState();
+}
+
+class _MyProjectState extends State<MyProject> {
+  projetCard(lang, title, description, star) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.9,
+      height: 220,
+      child: Card(
+        child: Container(
+          margin: EdgeInsets.only(left: 20, top: 30, right: 10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                lang,
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+              SizedBox(
+                height: 15,
+              ),
+              Text(
+                title,
+                style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                    fontWeight: FontWeight.w700),
+              ),
+              SizedBox(
+                height: 3,
+              ),
+              Text(
+                description,
+                style: TextStyle(color: Colors.white70, fontSize: 16),
+              ),
+              SizedBox(
+                height: 10,
+              ),
+              Row(
+                children: [
+                  Icon(
+                    Icons.star,
+                    color: Colors.white70,
+                    size: 18,
+                  ),
+                  SizedBox(
+                    width: 4,
+                  ),
+                  Text(
+                    star,
+                    style: TextStyle(
+                      color: Colors.white70,
+                    ),
+                  ),
+                  Expanded(child: Container()),
+                  IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        FontAwesomeIcons.github,
+                        color: Colors.white,
+                      )),
+                ],
+              )
+            ],
+          ),
+        ),
+        color: Color(0xff262628),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // backgroundColor: Color(0xff1e1e1e),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        backgroundColor: Color(0xff252525),
+        title: Text('Projects'),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: EdgeInsets.only(bottom: 20, top: 10),
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              projetCard('Node', 'AUpi', 'Create and download UPI QR', '10'),
+              projetCard('Python', 'Telegraph', 'Bot for Upload Img', '9'),
+              projetCard('HTML', 'Portfolio', 'Portfolio-Website.', '8'),
+              projetCard('Python', 'Bot', 'Remove-BG-Bot', '7'),
+              projetCard('CSS', 'MyBlog', 'MyBlog By Hugo', '10')
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
